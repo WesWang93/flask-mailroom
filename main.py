0e651f1d80d39a62db1461925d509a555e7fdc44
Wesley Wang <wes82@uw.edu>
2018-08-05 21:45:43 -0700

    Initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -11,7 +11,22 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations/')
+@app.route('/create', methods = ['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name==request.form['name']).get()
+        except:
+            donor = Donor(name=request.form['name'])
+            donor.save()
+        donation = Donation(value=request.form['donation'], donor=donor)
+        donation.save()
+
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
+
+@app.route('/donations')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
